ff8f828a538cfd205eca3dcbb292e6a9c6ed90a7
JT Patrick <jtpatrick01@gmail.com>
2024-02-05 14:54:56 -0700

    First time with a foreach loop

---
+++ Unit1C/ForEachLoop.cs
@@ -0,0 +1,13 @@
+using System;
+					
+public class Program
+{
+	public void Main()
+	{
+		String[] games = {"Halo", "Starcraft", "War Thunder", "Empires of the Undergrowth", "Call of Duty", "Star Wars: Empire at War", "Creeper World 4"};
+		foreach (var game in games)
+		{
+			Console.WriteLine(game);
+		}
+	}
+}
